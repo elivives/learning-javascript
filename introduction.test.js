23e8bca09ff3e76fe76cbed9260947c2cabb771a
elivives <eli@MacBook-de-Elisabet.local>
2022-10-16 22:49:44 +0200

    test and implementation of exercise b

--- introduction.test.js
+++ introduction.test.js
@@ -3,11 +3,18 @@ const intro = require('./introduction')
 
 describe('introduction suit', function(){
 
-    it('a) return you name', function(){
+    it('a) return your name', function(){
         var expectedMyName = ''
 
         expectedMyName = intro.getMyName()
 
         assert.equal(expectedMyName, 'Elisabet')
+    })
+    it('b) return your age', function(){
+        var expectedMyAge = 0
+
+        expectedMyAge = intro.getMyAge()
+
+        assert.equal(expectedMyAge, 25)
     })
 })
